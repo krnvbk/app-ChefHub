d621f7d6230aea9a269aef9dba07351e7393f460
emilearel <168546288+emilearel@users.noreply.github.com>
2024-09-12 18:39:06 -0500

    error de tipeo chefhub.sql
    Se eliminó una "," adicional

--- BD/chefhub.sql
+++ BD/chefhub.sql
@@ -6,7 +6,7 @@ CREATE TABLE usuarios (
     usuario_id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     correo_electronico VARCHAR(100) UNIQUE NOT NULL,
-    contrasena VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE recetas (
